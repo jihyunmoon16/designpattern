681964af9083665044cdd18aa0bd498b6d8c329f
jihyun <sugaryfood@naver.com>
2024-06-17 10:18:30 +0900

    initial commit

---
+++ src/main/java/com/moon/designpattern/creational/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.moon.designpattern.creational.singleton;
+
+/**
+ * <pre>
+ * @title  : Singleton 클래스
+ * @author : Moon Jihyun
+ * @since  : 2024-06-17
+ * @version 1.0
+ * @Modification
+ * 수정일          수정자              수정내용
+ * ------------------------------------------------------------------------------
+ * 2024-06-17    Moon Jihyun        Singleton 최초생성
+ * </pre>
+ */
+public class Singleton {
+}
